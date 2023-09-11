8f5f44df3afe10563ce25fd7cd0ab461b0fbd5f1
Emin Alemdar <77338109+eminalemdar@users.noreply.github.com>
2023-09-11 17:47:17 +0300

    Initial commit

---
+++ cluster.tf
@@ -0,0 +1,81 @@
+module "eks" {
+  source  = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v5.0.0"
+
+  cluster_name                   = var.cluster_name
+  cluster_version                = var.cluster_version
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns     = {}
+    kube-proxy  = {}
+    vpc-cni     = {
+      # Specify the VPC CNI addon should be deployed before compute to ensure
+      # the addon is configured before data plane compute resources are created
+      # See README for further details
+      before_compute = true
+      most_recent    = true # To ensure access to the latest settings provided
+      configuration_values = jsonencode({
+        env = {
+          ENABLE_NETWORK_POLICY = "true"
+
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+  }
+
+  vpc_id = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnets
+
+  node_security_group_additional_rules = {
+    # Extend node-to-node security group rules. Recommended and required for the Add-ons
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+
+    # Recommended outbound traffic for Node groups
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+    # Allows Control Plane Nodes to talk to Worker nodes on all ports. Added this to simplify the example and further avoid issues with Add-ons communication with Control plane.
+    # This can be restricted further to specific port based on the requirement for each Add-on e.g., metrics-server 4443, spark-operator 8080, karpenter 8443 etc.
+    # Change this according to your security requirements if needed
+    ingress_cluster_to_node_all_traffic = {
+      description                   = "Cluster API to Nodegroup all traffic"
+      protocol                      = "-1"
+      from_port                     = 0
+      to_port                       = 0
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+  }
+
+  managed_node_groups = {
+    node_group = {
+      node_group_name      = "managed-ondemand"
+      instance_types       = ["t3.large"]
+      subnet_ids           = module.vpc.private_subnets
+      force_update_version = true
+      min_size             = 1
+      max_size             = 1
+      desired_size         = 1
+    }
+  }
+
+  tags = {
+      Name = var.cluster_name
+  }
+}
